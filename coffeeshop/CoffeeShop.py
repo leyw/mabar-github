36f0d37cc309d6b01e11bd98c98eae55626bd498
Farhan Raditya <hellorisoau@gmail.com>
2024-10-01 23:24:49 +0800

    Update Coffeeshop app

--- coffeeshop/CoffeeShop.py
+++ coffeeshop/CoffeeShop.py
@@ -2,12 +2,23 @@ import Cappucino
 
 class CoffeeShop:
 
-    _recipes = []
+    _recipes = {};
 
     def register( self, coffeeName : str, make ):
-        self._recipes.append = make     
+        self._recipes[coffeeName] = make
+
+    def serve( self, coffeeName):
+        if coffeeName not in self._recipes:
+            return "Sorry, your coffee is currently unavailable"
+        
+        coffee =  self._recipes[coffeeName]
+        return coffee
 
 coffeeshop = CoffeeShop()
 cappucino_coffee = Cappucino.Cappucino()
 
 coffeeshop.register("cappucino", cappucino_coffee.serve)
+
+print(
+    coffeeshop.serve("cappucino")
+)
