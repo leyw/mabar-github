4d3df9be338da1e33c2781664c5b74b1c708add5
hellorisoau <hellorisoau@gmail.com>
2024-10-13 04:31:48 +0000

    login update

--- farhnnraal_daily_task/circle_area/index.php
+++ farhnnraal_daily_task/circle_area/index.php
@@ -1,4 +1,24 @@
-<!-- // Suggested code may be subject to a license. Learn more: ~LicenseLog:297887006. -->
+<?php
+
+    session_start();
+
+    if ( isset($_POST['logout']) )
+    {
+        session_unset();
+
+        session_destroy();
+
+        header('Location: ');
+    }
+
+    if ( !isset($_SESSION['status']) && $_SESSION['status'] != "session_approved" )
+    {
+        header('Location: ./login/');
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +27,12 @@
     <title>Circle Area Calculator</title>
 </head>
 <body>
+    <form action="" method="post">
+        <button type="submit" name="logout">Logout</button>
+    </form>
+
+    <br>
+
     <h3>Program Menghitung Luas Lingkaran</h3>
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
         <laber type="number" for="jari-jari">Berapa Jari-jarinya?</laber>
@@ -15,25 +41,31 @@
         <br>
         <button type="submit">Hitung Sekarang!</button>
     </form>
-
-    <?php
-
-        function countArea( $jari_jari )
-        {
-            $luas = 3.14 * pow($jari_jari, 2);
-            echo "Luas lingkarannya dari jari-jari = $jari_jari adalah: $luas";
-        }
-
-        $jari_jari = isset($_POST['jarijari']) ? $_POST['jarijari'] : 0;
-
-        if ($jari_jari != NULL)
-        {
-            countArea( $jari_jari );    
-        } else
-        {
-            echo "Angka nya mana le?";
-        }
-
-    ?>
 </body>
 </html>
+
+<?php
+
+    function countArea( $jari_jari )
+    {
+        $luas = 3.14 * pow($jari_jari, 2);
+        echo "Luas lingkarannya dari jari-jari = $jari_jari adalah: $luas";
+    }
+
+    $jari_jari = isset($_POST['jarijari']) ? $_POST['jarijari'] : 0;
+
+    if ($jari_jari != NULL)
+    {
+        countArea( $jari_jari );    
+    } else
+    {
+        echo "Angka nya mana le?";
+    }
+
+    if (isset($_POST['jarijari'])) {
+        $jariJari = $_POST['jarijari'];
+        $luas = pi() * pow($jariJari, 2);
+        echo "Luas lingkaran dengan jari-jari $jariJari adalah: $luas";
+    } else {
+        echo "Masukkan jari-jari lingkaran!";
+    }
--- farhnnraal_daily_task/circle_area/login/login.php
+++ farhnnraal_daily_task/circle_area/login/login.php
@@ -1,64 +1,64 @@
+<?php
+
+    session_start();
+
+?>
+
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Login</title>    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Login Document</title>
 </head>
 <body>
-    <div class="container">
-
+    <div class="container p-3">
         <h1>Login</h1>
-
-        <form method="post" action="process_login.php">
-
-            <class class="row-auto">
-                <class class="col-auto">
-                    <label class="form-label" for="username">Username:</label>
-                </class>
-            </class>
-
-            <class class="row-auto">
-                <class class="col-auto">
-                    <input class="form-control" type="text" id="username" name="username" aria-describedby="usernameHelp" required>
-                </class>
-                <class class="col-auto">
-                    <span id="usernameHelp" class="form-text">
-                        <a href="#">Forgot username?</a>
-                    </span>
-                </class>
-            </class>
-
-            <class class="row-auto">
-                <class class="col-auto">
-                    <label class="form-label" for="password">Password:</label>
-                </class>
-            </class>
-
-            <class class="row-auto">
-                <class class="col-auto">
-                    <label class="form-label" for="password">Password:</label>
-                </class>
-            </class>
-
-            <class class="row-auto">
-                <class class="col-auto">
-                    <input class="form-control" type="password" id="password" name="password" aria-describedby="passwordHelp" required>
-                </class>
-                <class class="col-auto">
-                    <span id="passwordHelp" class="form-text">
-                        <a href="#">Forgot password?</a>
-                    </span>
-                </class>
-            </class>
-    
-            <class class="row-auto">
-                <class class="col-auto">
-                    <button type="submit">Login</button>
-                </class>
-            </class>
-
-        </form>
         
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+
+            <div class="row align-items-center p-2">
+                <div class="col">
+                    <input class="form-control" type="text" id="username" name="username" placeholder="Username" aria-label="Username" aria-describedby="usernameHelp" required>
+                </div>
+
+                <div id="usernameHelp" class="form-text col">
+                    <span>Forgot Username?</span>
+                </div>
+            </div>
+            
+            <div class="row align-items-center p-2">
+                <div class="col">
+                    <input class="form-control" type="password" id="password" name="password" placeholder="Password" aria-label="Password" aria-describedby="passwordHelp" required>
+                </div>
+
+                <div id="passwordHelp" class="form-text col">
+                    <span>Forgot Password?</span>
+                </div>
+            </div>
+    
+            <div class="row-auto align-items-center p-2">
+                <button class="btn btn-primary" type="submit">Login</button>
+            </div>
+    
+        </form>
     </div>
+
 </body>
-</html>
+</html>    
+
+    $account = [
+        "username" => "hello",
+        "password" => "hi"
+    ];
+
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        if ($_POST['username'] === $account['username'] && $_POST['password'] === $account['password']) {
+            $_SESSION['status'] = 'session_approved';
+            header('Location: ../');
+        } else {
+            echo 'Login failed!';
+        }
+    }
